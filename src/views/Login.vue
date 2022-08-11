cbd7e164bf6a5ade31a3bf828aafeb4ae71c9a13
lvjingxin <1768538073@qq.com>
2022-08-11 15:37:05 +0800

    功能登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,21 +4,21 @@
       <template #header>面经运营后台</template>
 
       <!-- autocomplete: auto自动  complete完成  -->
-      <el-form autocomplete="off" :model="form" ref="form" :rules="rules">
+      <el-form autocomplete="off" :model="form" ref="box" :rules="rules">
         <el-form-item label="用户名" prop="username">
           <el-input placeholder="输入用户名" v-model="form.username"></el-input>
         </el-form-item>
 
-        <el-form-item label="密码">
+        <el-form-item label="密码" prop="password">
           <el-input
             type="password"
             placeholder="输入用户密码"
-            v-model="password"
+            v-model="form.password"
           ></el-input>
         </el-form-item>
 
         <el-form class="tc">
-          <el-button type="primary">登 录</el-button>
+          <el-button type="primary" @click="login">登 录</el-button>
           <el-button>重 置</el-button>
         </el-form>
       </el-form>
@@ -30,6 +30,15 @@
 export default {
   name: "login-page",
   data() {
+    let fn = (rule, value, callback) => {
+      let res = /^[a-zA-Z0-9]{5,11}$/;
+      if (res.test(value) == false) {
+        // console.log(res)
+        callback(new Error("请输入密码"));
+      } else {
+        callback();
+      }
+    };
     return {
       form: {
         username: "",
@@ -37,14 +46,35 @@ export default {
       },
       rules: {
         username: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },//trigger   触发
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: true, message: "请输入用户名", trigger: "blur" }, //trigger   触发
+          { min: 5, max: 11, message: "长度在 5 到 11 个字符", trigger: "blur" },
         ],
-        
+        password: [
+          { required: true, message: "请输入正确密码", trigger: "blur" },
+          { validator: fn, trigger: "blur" },
+        ],
       },
     };
   },
-  methods: {},
+  methods: {
+    login() {
+      this.$refs.box.validate( async (valid) => {
+        if (valid) {//验证通过
+          // 发送请求
+          let res = await this.$axios({
+            url:'/auth/login',
+            method:'POST',
+            data:this.form
+          })
+          localStorage.setItem('pc-token','Bearer '+res.data.data.token)
+          this.$router.push('/dashboard')
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+  },
 };
 </script>
 
